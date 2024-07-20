ad4519afbc71a088b5f814aad124ecb5195ee47b
NightPotato <NightPotato@users.noreply.github.com>
2024-07-20 11:48:15 -0400

    Implement basic db_dependency injection, create_account logic, and token logic.

--- routers/Auth.py
+++ routers/Auth.py
@@ -2,7 +2,19 @@
 # REST API authentication using OAuth2 or Basic Authentication
 #
 
-from fastapi import APIRouter, Request, Response
+from datetime import timedelta, datetime, timezone
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
+import os
+
+from models.User import User
+from Database import SessionLocal
 
 
 router = APIRouter(
@@ -10,8 +22,64 @@ router = APIRouter(
     tags=['Authentication']
 )
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALGORITHM = os.environ.get('ALGORITHM')
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
-@router.post('/login')
-async def process_login():
-    pass
+class CreateUserRequest(BaseModel):
+    email: str
+    password: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.post('/createAccount', status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    """
+     Create a new User account for accessing the API.
+    """
+    create_user_model = User(
+        email=create_user_request.email,
+        hashed_password=bcrypt_context.hash(create_user_request.password)
+    )
+
+    # TODO: Add some more error checking within this method.
+
+    db.add(create_user_model)
+    db.commit()
+    return {"status": 'Successfully created a new user account.'}
+
+@router.post('/token', response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db:db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user.')
+
+    token = create_access_token(user.email, user.id, timedelta(minutes=30))
+    return {'access_token': token, 'token_type': 'bearer'}
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(User).filter(User.email == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(email: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub': email, 'id': user_id}
+    expires = datetime.now(timezone.utc)
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
